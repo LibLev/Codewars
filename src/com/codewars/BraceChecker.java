b60c8bf63968a91b1ce35bea23393719de84e19f
LibLev <libardi.levente@gmail.com>
2021-02-16 08:02:23 +0100

    BraceChecker codewars kata has been solved

--- src/com/codewars/BraceChecker.java
+++ src/com/codewars/BraceChecker.java
@@ -1,15 +1,39 @@
 package com.codewars;
 
-public class BraceChecker {
+import java.util.*;
 
+
+public class BraceChecker {
     public boolean isValid(String braces) {
-        char[] chars = braces.toCharArray();
-        for (int i = 1; i < chars.length; i++) {
-            if (chars[i-1] == '(' &&  chars[i-1] == ')'){
-                return true;
+        String[] arr = braces.split("");
+        Stack<String> s = new Stack<>();
+        for(String i : arr){
+            if(i.equals("(")||i.equals("{")||i.equals("[")){
+                s.push(i);
+            }
+            else{
+                if(s.empty()){
+                    return false;
+                }
+                if(i.equals("]")&& s.peek().equals("[")){
+                    s.pop();
+                    continue;
+                }
+                if(i.equals(")")&& s.peek().equals("(")){
+                    s.pop();
+                    continue;
+                }
+                if(i.equals("}")&& s.peek().equals("{")){
+                    s.pop();
+                    continue;
+                }
+                else{
+                    return false;
+                }
             }
         }
-        return false;
+        return (s.empty());
     }
-
 }
+
+
